18b53510436296948c51a69904f112cde8bd3490
Łukasz Chruściel <lchrusciel@gmail.com>
2018-04-18 15:50:33 +0200

    custom-menu

---
+++ src/AppBundle/EventListener/AdminMenuListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class AdminMenuListener
+{
+    public function addSupplierToMenu(MenuBuilderEvent $menuBuilderEvent): void
+    {
+        $menu = $menuBuilderEvent->getMenu();
+
+        $supplierSubmenu = $menu
+            ->getChild('catalog')
+        ;
+
+        $supplierSubmenu
+            ->addChild('supplier_index', [
+                'route' => 'app_admin_supplier_index',
+            ])
+            ->setLabel('Manage Suppliers')
+            ->setLabelAttribute('icon', 'sliders')
+        ;
+    }
+}
