17e48c713a3a02b4592563d6b86acfe724167183
Łukasz Chruściel <lchrusciel@gmail.com>
2018-04-20 10:44:11 +0200

    Generate coupon for customer

---
+++ src/AppBundle/EventListener/OrderFinishedCouponGeneratorListener.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Sylius\Component\Core\Factory\PromotionActionFactoryInterface;
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\Model\PromotionInterface;
+use Sylius\Component\Core\Repository\PromotionRepositoryInterface;
+use Sylius\Component\Promotion\Generator\PromotionCouponGeneratorInstruction;
+use Sylius\Component\Promotion\Generator\PromotionCouponGeneratorInterface;
+use Sylius\Component\Resource\Factory\Factory;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+final class OrderFinishedCouponGeneratorListener
+{
+    private const ORDER_PLACED_PROMOTION = 'ORDER_PLACED_PROMOTION';
+
+    /** @var FactoryInterface */
+    private $factory;
+
+    /** @var PromotionRepositoryInterface */
+    private $repository;
+
+    /** @var PromotionCouponGeneratorInterface */
+    private $promotionCouponGenerator;
+    /** @var PromotionActionFactoryInterface */
+    private $promotionActionFactory;
+
+    public function __construct(
+        FactoryInterface $factory,
+        PromotionRepositoryInterface $repository,
+        PromotionActionFactoryInterface $promotionActionFactory,
+        PromotionCouponGeneratorInterface $promotionCouponGenerator
+    ) {
+        $this->factory = $factory;
+        $this->promotionCouponGenerator = $promotionCouponGenerator;
+        $this->repository = $repository;
+        $this->promotionActionFactory = $promotionActionFactory;
+    }
+
+    public function sendCoupon(GenericEvent $event): void
+    {
+        $order = $event->getSubject();
+
+        if (!$order instanceof OrderInterface) {
+            return;
+        }
+        $promotion = $this->providePromotion($order);
+
+
+        $generatorInstruction = new PromotionCouponGeneratorInstruction();
+        $generatorInstruction->setAmount(1);
+        $generatorInstruction->setUsageLimit(1);
+
+        $this->repository->add($promotion);
+
+        $this->promotionCouponGenerator->generate($promotion, $generatorInstruction);
+    }
+
+    private function providePromotion(OrderInterface $order): PromotionInterface
+    {
+        $promotion = $this->repository->findOneBy(['code' => self::ORDER_PLACED_PROMOTION]);
+
+        if ($promotion !== null) {
+            return $promotion;
+        }
+
+        /** @var PromotionInterface $promotion */
+        $promotion = $this->factory->createNew();
+
+        $channel = $order->getChannel();
+
+        $promotion->setName('Order placed promotion');
+        $promotion->setCode(self::ORDER_PLACED_PROMOTION);
+        $promotion->setCouponBased(true);
+        $promotion->addChannel($channel);
+        $promotion->addAction($this->promotionActionFactory->createFixedDiscount(500, $channel->getCode()));
+
+        return $promotion;
+    }
+}
